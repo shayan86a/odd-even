f3a312abb48d85a0fed016ed3c0801aa1f7ece73
shayan86a <111562719+shayan86a@users.noreply.github.com>
2022-08-19 02:35:50 -0700

    Add files via upload

---
+++ odd & even.py
@@ -0,0 +1,8 @@
+num1 = input("please insert the number: ")
+result = float(num1) % 2
+# if result == 0:
+#     print("Even number!")
+# else:
+#     print("odd number!")
+print("even") if result == 0 else print("odd")
+
